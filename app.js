2899578e8fbb8cd2dd71e80d1524773c4f6dc25f
sanDream <fs517010856@gmail.com>
2022-08-25 22:04:46 +0800

    add 每次进入小程序时会获取openid并保存在缓存

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 // app.js
-App({})
+const request = require('./utils/request')
+App({
+  onLaunch() {
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if(res.code) {
+          request.get('/wxapp/login/getOpenid?code=' + res.code).then((data) => {
+            console.log('data', data)
+            if (data.data && data.data.openid){
+              wx.setStorageSync('openid', data.data.openid)
+            }
+          }, (err) => {
+            console.log('err', err)
+          })
+        }
+      }
+    })
+  },
+  clearSession() {
+    wx.removeStorageSync('auth_token')
+  },
+  setSession(value) {
+    wx.setStorageSync('auth_token', value)
+  },
+  globalData: {
+    baseUrl: 'http://192.168.63.21'
+  }
+})
